957568fa6bd68dbee04969170c8fa73831a79502
dineshkila <dineshkvcod@rediffmail.com>
2016-01-31 14:15:18 +0530

    Added main print method.

---
+++ NewGit.java
@@ -0,0 +1,10 @@
+public class NewGit{
+	public static void test(){
+			System.out.println("testing NewGit");
+		}
+
+	public static void main(String[] args){
+			test();
+			System.out.println("Inside Main Method");
+	}
+}
